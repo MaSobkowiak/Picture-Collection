8dc0e892a51797e4b1d2fa979ca1831f8b877fae
Maciej Sobkowiak <masobkowiak@gmail.com>
2021-05-28 01:19:56 +0200

    Read folder info.json data

--- src/helpers/__init__.py
+++ src/helpers/__init__.py
@@ -2,6 +2,6 @@ from ._config import get_config
 from ._log import setup_logging
 from ._db_sqlite import SQLite
 from ._db_mssql import MSSQL
-from ._paths import get_folders
+from ._paths import get_folders, get_folder_info
 from ._geo import get_geotags
 from ._backup import backup
--- src/helpers/_paths.py
+++ src/helpers/_paths.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import json
+import logging
 from ._config import get_config
 
 
@@ -9,3 +11,33 @@ def get_folders(path=None):
         path = Path(get_config("paths")["photos"])
 
     return [folder for folder in path.iterdir() if folder.is_dir()]
+
+def check_if_key_in_dict(dict, key):
+    if key in dict:
+        return dict[key]
+    else:
+        if key == 'geoTags':
+            return []
+        else:
+            return None
+
+def get_folder_info(path):
+    """ Return dict of info.json in folder directory """
+
+    logger = logging.getLogger(str("Folder: " + path.name))
+
+    try:
+        if Path(path, 'info.json').is_file():
+            with open(Path(path, 'info.json'), encoding='utf-8') as json_info:
+                
+                data = json.loads(json_info.read())
+                
+                return check_if_key_in_dict(data, 'title'), check_if_key_in_dict(data, 'description'),  check_if_key_in_dict(data, 'geoTags')
+        else:
+            logger.warning("Cant find info.json for folder: " +
+                    str(path))
+            return None, None, []
+
+    except (OSError, KeyError) as e:
+        logger.error("Failed to load info.json file: " +
+                     str(path.parent) + " " + str(e))
